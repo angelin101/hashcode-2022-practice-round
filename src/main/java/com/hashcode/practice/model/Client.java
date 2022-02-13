e6d0b419b65e6049edd2230f7845f09f6d3cd9f6
Dmitry Gorobets <dmitry.gorobets@axispoint.com>
2022-02-13 23:00:42 +0200

    initial reader impl

--- src/main/java/com/hashcode/practice/model/Client.java
+++ src/main/java/com/hashcode/practice/model/Client.java
@@ -2,11 +2,16 @@ package com.hashcode.practice.model;
 
 import lombok.Data;
 
+import java.util.Collections;
 import java.util.List;
 
 @Data
 public class Client {
-    List<String> likes;
-    List<String> dislikes;
+    List<String> likes = Collections.emptyList();
+    List<String> dislikes = Collections.emptyList();
 
+    @Override
+    public String toString() {
+        return String.format("likes: %s %s; dislikes: %s %s", likes.size(), String.join(" ", getLikes()), dislikes.size(), String.join(" ", getDislikes()));
+    }
 }
--- src/main/java/com/hashcode/practice/service/Reader.java
+++ src/main/java/com/hashcode/practice/service/Reader.java
@@ -1,17 +1,46 @@
 package com.hashcode.practice.service;
 
+import com.hashcode.practice.model.Client;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Reader {
 
-    public void read(){
-        try (Stream<String> stream = Files.lines(Paths.get("fileName"))) {
-            stream.forEach(System.out::println);
+    public List<Client> read() {
+        List<Client> clients = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get("/Users/user/Documents/Learning/HashCodeCompetetion/Practice/hashcode-2022/src/main/resources/inputData/b_.txt"))) {
+            List<String> lines = stream.collect(Collectors.toList());
+
+            for (int i = 1; i < lines.size() - 1; i += 2) {
+                clients.add(mapClientFields(parseLine(lines.get(i)), parseLine(lines.get(i + 1))));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(clients);
+        return clients;
     }
+
+    Client mapClientFields(List<String> likes, List<String> dislikes) {
+        Client client = new Client();
+        client.setLikes(likes);
+        client.setDislikes(dislikes);
+        return client;
+    }
+
+    List<String> parseLine(String ingrediens) {
+        Optional<String> optStr = Optional.ofNullable(ingrediens);
+        return optStr.map(s -> Arrays.stream(s.split(" ")).skip(1).collect(Collectors.toList())).orElse(Collections.emptyList());
+    }
+
+
 }
